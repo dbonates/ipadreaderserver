5c10ee67e705c3cef581fd5662aa049191ef0c9d
Daniel Bonates <dbonates@gmail.com>
2013-04-08 04:23:39 -0300

    fine tunning deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ load "config/recipes/mysql"
 # load "config/recipes/check"
 
 ssh_options[:port] = 63
+ssh_options[:forward_agent] = true
 
 server "bonates.com", :web, :app, :db, primary: true
 
@@ -19,10 +20,24 @@ set :deploy_via, :remote_cache
 set :use_sudo, true
 
 set :scm, "git"
-# set :local_repository, "."
-set :repository, "https://github.com/dbonates/ipadreaderserver.git"
+set :repository, "git@github.com:dbonates/#{application}.git"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "deploy:cleanup" # keep only the last 5 releases  
+
+namespace :deploy do
+  
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+      puts "WARNING: HEAD is not the same as origin/#{branch}"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+  
+end
