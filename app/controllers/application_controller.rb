12ac08766ef62b2fd2c637cd14670b11cad5a478
Daniel Bonates <danielbonates@dbombp.local>
2013-05-31 03:17:02 -0300

    notifications bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,4 +34,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   
+  private
+
+
+
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-        Ipadreaderserver::Application.configure do
+Ipadreaderserver::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
